dd8e3a0af168261b67684bfae833e9d349982f3a
Stephanos Kuma <stephanos.papanikolopoulos@oaknorth.ai>
2019-12-30 16:48:36 +0000

    Remove outdated test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,13 +7,6 @@ from yashiro import main
 class TestMain:
     @staticmethod
     @mock.patch("yashiro.main.parse_args")
-    def test_version(mocked_args_parser):
-        args = mocked_args_parser()
-        args.version = True
-        assert main.get_output().startswith("Yashiro ")
-
-    @staticmethod
-    @mock.patch("yashiro.main.parse_args")
     def test_parser(mocked_args_parser, data_path):
         args = mocked_args_parser()
         args.template = data_path("test.template")
