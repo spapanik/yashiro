f1d07c3750bc197e47c2627e3064f236e3c61207
Stephanos Kuma <stephanos@kuma.ai>
2024-05-22 14:11:21 +0100

    🔄 Make template a positional argument

---
+++ src/yashiro/command/yashiro.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+import jinja2
+from dj_settings import ConfigParser
+
+
+class Command:
+    __slots__ = ("template", "mapping")
+
+    def __init__(self, template: Path, mapping: Path | None, *, strict: bool):
+        raw_template = template.read_text()
+        undefined = jinja2.StrictUndefined if strict else jinja2.Undefined
+        self.template = jinja2.Template(raw_template, undefined=undefined)
+        self.mapping = dict(os.environ)
+        if mapping is not None:
+            data = ConfigParser([Path(mapping)]).data
+            self.mapping |= data.get("yashiro", {})
+
+    def render(self) -> str:
+        return self.template.render(self.mapping)
+
+    def write(self) -> None:
+        print(self.render())
